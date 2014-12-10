8aa14364b862ac42ed5c8da25935bfc338e1833f
Bruno Tavares <bruno.exz@gmail.com>
2014-12-10 21:53:21 -0200

    Final do dojo

--- spec/dojo/core_spec.clj
+++ spec/dojo/core_spec.clj
@@ -1,7 +1,21 @@
 (ns dojo.core-spec
-  (:require [speclj.core :refer :all]
-            [dojo.core :refer :all]))
+  (:require [speclj.core :exclude [run-specs] :refer :all]
+            [dojo.core :refer :all]
+            [speclj.run.standard :refer [run-specs]]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "dojo"
+          (it "faz tudo"
+              (should= (dojo2 square (fn [x] (>= 40 x))) 6))
+          (it "faz tudo"
+              (should= (dojo2 square #(>= 40 %1)) 6))
+          (it "faz tudo"
+              (should= (dojo square (partial >= 40)) 6))
+          (it "faz tudo"
+            (should= (dojo2 cube (partial >= 40)) 3))
+
+          (it "faz tudo"
+              (should= (into [] (from-1-until square (partial >= 40)) (range)) []))
+
+          )
+
+(run-specs)
--- src/dojo/core.clj
+++ src/dojo/core.clj
@@ -1,1 +1,27 @@
 (ns dojo.core)
+
+(defn square [x]
+  (* x x))
+
+(defn cube [x]
+  (* x x x))
+
+(defn from-1-until [f pred]
+  (comp
+    (drop 1)
+    (map f)
+    (take-while pred)))
+
+(defn dojo [f pred]
+  (transduce (comp
+               (from-1-until f pred)
+               (map (constantly 1)))
+             + 0 (range)))
+
+(defn dojo2 [f pred]
+  (->> (range) ; => range infinito comecando em 0
+         (drop 1) ; => Range iniciando do 1
+         (map f) ; => numeros ate o quadrado
+         (take-while pred)
+         (map (constantly 1)) ; => Transforma tudo pra 1
+         (reduce + 0))) ; => Soma tudo
